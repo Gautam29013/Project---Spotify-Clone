7804daff0ea505d93e7642a8868e144c3fc8ed84
Gautam29013 <jonygautam29013@gmail.com>
2026-02-11 20:36:38 +0530

    script work

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Lets write JavaScript")
+
+
+
+
+async function getSongs() {
+    let a = wait fetch()
+    let response = await a.text;
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            sessionStorage.push(element.href)
+        }
+    }
+    return songs
+
+}
+
+
+async function main() {
+    let songs = await getSongs()
+    console.log(songs)
+
+}
+
+main()
