b5b1ac9086b816894398f84bc8d377101acc6aa2
Gautam29013 <jonygautam29013@gmail.com>
2026-02-18 10:42:30 +0530

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ console.log("Lets write JavaScript")
 
 
 async function getSongs() {
-    let a = wait fetch()
-    let response = await a.text;
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
-
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            sessionStorage.push(element.href)
+            songs.push("/songs/" + element.href.split("/").pop())
         }
     }
     return songs
-
 }
 
 
@@ -27,7 +26,7 @@ async function main() {
     console.log(songs)
 
 
-    var audio = new audio(songs[0]);
+    var audio = new Audio(songs[0]);
     audio.play();
 
     audio.addEventListener("loadeddata", () => {
